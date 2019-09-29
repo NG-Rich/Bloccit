5b21d2e46de54b00326ec487c104c71ad0e913cb
Richard Chan <rchan817@yahoo.com>
2019-09-28 21:51:11 -0700

    assignment 4 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const aboutUs = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
@@ -9,6 +10,16 @@ describe("routes : static", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
+        done();
+      });
+    });
+  });
+
+  describe("GET /about", () => {
+    it("should return status code 200 and have the string 'About Us' in the body", (done) => {
+      request.get(aboutUs, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
         done();
       });
     });
