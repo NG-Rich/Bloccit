d9f45f5e7a870e2fe88c90a53e02cccd142d5d1e
Richard Chan <rchan817@yahoo.com>
2019-10-14 23:40:37 -0700

    checkpoint 10 completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -82,6 +82,28 @@ describe("routes : post", () => {
 
     });
 
+    it("should not create a new post that fails validation", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options, (err, res, body) => {
+        Post.findOne({where: {title: "a"}})
+        .then((post) => {
+          expect(post).toBeNull();
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+
   });
 
   describe("GET /topics/:topicId/posts/:id", () => {
@@ -145,7 +167,8 @@ describe("routes : post", () => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "I really enjoy the funny hats on them."
         }
       };
 
@@ -160,7 +183,7 @@ describe("routes : post", () => {
           done();
         });
       });
-      
+
     });
 
   });
