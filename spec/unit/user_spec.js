26f8046cdaacd2ad7fb7d0e9893f8fbc392f6473
Richard Chan <rchan817@yahoo.com>
2019-10-16 01:20:13 -0700

    checkpoint 11 completed

---
+++ spec/unit/user_spec.js
@@ -0,0 +1,78 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const User = require("../../src/db/models").User;
+
+describe("User", () => {
+
+  beforeEach((done) => {
+    sequelize.sync({force: true})
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+
+  describe("#create()", () => {
+
+    it("should create a User object with a valid email and password", (done) => {
+      User.create({
+        email: "user@example.com",
+        password: "1234567890"
+      })
+      .then((user) => {
+        expect(user.email).toBe("user@example.com");
+        expect(user.id).toBe(1);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+    it("should not create a User with an invalid email or password", (done) => {
+      User.create({
+        email: "It's-a me, Mario!",
+        password: "1234567890"
+      })
+      .then((user) => {
+        // This block won't be evaluated and be skipped. Set expectations in catch
+        done();
+      })
+      .catch((err) => {
+        expect(err.message).toContain("Validation error: must be a valid email");
+        done();
+      });
+    });
+
+    it("should not create a User with an email already taken", (done) => {
+      User.create({
+        email: "user@example.com",
+        password: "1234567890"
+      })
+      .then((user) => {
+        User.create({
+          email: "user@example.com",
+          password: "nananananana BATMAN!"
+        })
+        .then((user) => {
+          // This block will skip
+          done();
+        })
+        .catch((err) => {
+          expect(err.message).toContain("Validation error");
+          done();
+        });
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+  });
+
+});
